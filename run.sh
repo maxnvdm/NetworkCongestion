a24ac577f70c9e049fa1f2cf45ac285316632db4
maxnvdm <maxnvdm@users.noreply.github.com>
2019-02-27 18:41:27 +0200

    run.sh added

---
+++ run.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+time=20
+bwnet=2
+
+delay=5
+
+qsize=100
+dir=reno-q$qsize
+python tcp.py --bw-net $bwnet --delay $delay --dir $dir --time $time --maxq $qsize
+python plot_queue.py -f $dir/q.txt -o $dir/q.png
+python plot_ping.py -f $dir/ping.txt -o $dir/rtt.png
+
+qsize=20
+dir=reno-q$qsize
+python tcp.py --bw-net $bwnet --delay $delay --dir $dir --time $time --maxq $qsize
+python plot_queue.py -f $dir/q.txt -o $dir/q.png
+python plot_ping.py -f $dir/ping.txt -o $dir/rtt.png
+
+qsize=100
+dir=cubic-q$qsize
+python tcp.py --bw-net $bwnet --delay $delay --dir $dir --time $time --maxq $qsize --cong cubic
+python plot_queue.py -f $dir/q.txt -o $dir/q.png
+python plot_ping.py -f $dir/ping.txt -o $dir/rtt.png
+
+qsize=20
+dir=cubic-q$qsize
+python tcp.py --bw-net $bwnet --delay $delay --dir $dir --time $time --maxq $qsize --cong cubic
+python plot_queue.py -f $dir/q.txt -o $dir/q.png
+python plot_ping.py -f $dir/ping.txt -o $dir/rtt.png
+
+qsize=100
+dir=bbr-q$qsize
+python tcp.py --bw-net $bwnet --delay $delay --dir $dir --time $time --maxq $qsize --cong bbr --qman fq
+python plot_queue.py -f $dir/q.txt -o $dir/q.png
+python plot_ping.py -f $dir/ping.txt -o $dir/rtt.png
+
+qsize=20
+dir=bbr-q$qsize
+python tcp.py --bw-net $bwnet --delay $delay --dir $dir --time $time --maxq $qsize --cong bbr --qman fq
+python plot_queue.py -f $dir/q.txt -o $dir/q.png
+python plot_ping.py -f $dir/ping.txt -o $dir/rtt.png
