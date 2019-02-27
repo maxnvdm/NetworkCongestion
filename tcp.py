bd9933dccbce19d66c5a4590b990b8cfa88ab9b6
maxnvdm <maxnvdm@gmail.com>
2019-02-27 13:26:51 +0200

    Filled in the required sections on tcp.py

--- tcp.py
+++ tcp.py
@@ -22,7 +22,7 @@ import termcolor as T
 import sys
 import os
 import math
-
+import numpy
 
 # TODO: Don't just read the TODO sections in this code.  Remember that
 # one of the goals of this lab is for you to learn how to use
@@ -83,9 +83,12 @@ class TCPTopo(Topo):
         # interface names will change from s0-eth1 to newname-eth1.
         switch = self.addSwitch('s0')
         # TODO: Add links with appropriate characteristics
-        
-        
-        
+        h1 = self.addHost('h1')
+        self.addLink( h1, switch, bw=args.bw_host, delay=args.delay,
+                          max_queue_size=args.maxq)
+        h2 = self.addHost('h2')
+        self.addLink( h2, switch, bw=args.bw_net, delay=args.delay,
+                          max_queue_size=args.maxq)
         return
 
 # Simple wrappers around monitoring utilities.  You are welcome to
@@ -109,22 +112,45 @@ def start_iperf(net):
     
     # TODO: Retrieve the hosts, replace with appropriate names
     h1 = net.get('h1')
+    h2 = net.get('h2')
     
     
     print("Starting iperf server...")
     # For those who are curious about the -w 16m parameter, it ensures
     # that the TCP flow is not receiver window limited.  If it is,
     # there is a chance that the router buffer may not get filled up.
-    server = h1.popen("iperf -s -w 16m")
+    server1 = h1.popen("iperf -s -w 16m")
+    server2 = h2.popen("iperf -s -w 16m")
     # TODO: Start the iperf client on h1 and h2.  Ensure that you create two
     # long lived TCP flows in both directions.
-    
-    
+    client2 = h2.popen("iperf -c %s -t %d" % (h1.IP(), args.time + 5))
+    client1 = h1.popen("iperf -c %s -t %d" % (h2.IP(), args.time + 5))
+
 def start_webserver(net):
     server = net.get('h1')
     proc = server.popen("python http/webserver.py", shell=True)
     sleep(1)
     return [proc]
+
+def fetch_webpage(net):
+    print("Fetching webpage")
+    fetch_times =[]
+    h1 = net.get("h1")
+    h2 = net.get("h2")
+    start_time = time()
+    while True:
+        # do the measurement (say) 3 times.
+        sleep(5)
+        now = time()
+        delta = now - start_time
+        if delta > args.time:
+            break
+        print("%.1fs left..." % (args.time - delta))
+        fetch = h2.popen("curl -o /dev/null -s -w %%{time_total} %s/http/index.html"
+                         % h1.IP())
+        fetch.wait()
+        fetch_times.append(float(fetch.communicate()[0]))
+    return fetch_times
 
 def start_ping(net):
     # TODO: Start a ping train from h1 to h2 (or h2 to h1, does it
@@ -134,7 +160,10 @@ def start_ping(net):
     # Hint: Use host.popen(cmd, shell=True).  If you pass shell=True
     # to popen, you can redirect cmd's output using shell syntax.
     # i.e. ping ... > /path/to/ping.
-    
+    print "Starting ping train..."
+    h1 = net.get('h1')
+    h2 = net.get('h2')
+    ping = h1.popen("ping -i 0.1 %s > %s/ping.txt" % (h2.IP(), args.dir), shell=True)
     
   
 
@@ -164,33 +193,24 @@ def tcp():
                       outfile='%s/q.txt' % (args.dir))
 
     # TODO: Start iperf, webservers, etc.
-    
-    
+    start_iperf(net)
+    start_webserver(net)
+    start_ping(net)
 
     # TODO: measure the time it takes to complete webpage transfer
     # from h1 to h2 (say) 3 times.  Hint: check what the following
     # command does: curl -o /dev/null -s -w %{time_total} google.com
     # Now use the curl command to fetch webpage from the webserver you
     # spawned on host h1 (not from google!)
-
+    fetch_times = fetch_webpage(net)
     # Hint: have a separate function to do this and you may find the
     # loop below useful.
-    
-    start_time = time()
-    while True:
-        # do the measurement (say) 3 times.
-       
-        sleep(5)
-        now = time()
-        delta = now - start_time
-        if delta > args.time:
-            break
-        print("%.1fs left..." % (args.time - delta))
     
        
     # TODO: compute average (and standard deviation) of the fetch
     # times.  
-
+    print "Average fetch time: %f\n" % numpy.average(fetch_times)
+    print "Standard deviation of fetch times: %f\n" % numpy.std(fetch_times)
     # Hint: The command below invokes a CLI which you can use to
     # debug.  It allows you to run arbitrary commands inside your
     # emulated hosts h1 and h2.
@@ -198,6 +218,8 @@ def tcp():
 
     stop_tcpprobe()
     qmon.terminate()
+    Popen("pgrep -f ping | xargs kill -9", shell=True).wait()
+    Popen("pgrep -f iperf | xargs kill -9", shell=True).wait()
     net.stop()
     # Ensure that all processes you create within Mininet are killed.
     # Sometimes they require manual killing.
