0ee5c1e5444723520bbf299cab82dc16695609ff
maxnvdm <maxnvdm@gmail.com>
2019-02-28 16:59:38 +0200

    Typo in run.sh

--- run.sh
+++ run.sh
@@ -11,7 +11,7 @@ python tcp.py --bw-net $bwnet --delay $delay --dir $dir --time $time --maxq $qsi
 python plot_queue.py -f $dir/q.txt -o $dir/qreno1.png
 python plot_ping.py -f $dir/ping.txt -o $dir/rttreno1.png
 
-python tcp.py --bw-host 100--bw-net $bwnet --delay $delay --dir $dir --time $time --maxq $qsize
+python tcp.py --bw-host 100 --bw-net $bwnet --delay $delay --dir $dir --time $time --maxq $qsize
 python plot_queue.py -f $dir/q.txt -o $dir/renoq.png
 python plot_ping.py -f $dir/ping.txt -o $dir/renortt.png
 
@@ -27,7 +27,7 @@ python tcp.py --bw-net $bwnet --delay $delay --dir $dir --time $time --maxq $qsi
 python plot_queue.py -f $dir/q.txt -o $dir/qcubic1.png
 python plot_ping.py -f $dir/ping.txt -o $dir/rttcubic1.png
 
-python tcp.py --bw-host 100--bw-net $bwnet --delay $delay --dir $dir --time $time --maxq $qsize --cong cubic
+python tcp.py --bw-host 100 --bw-net $bwnet --delay $delay --dir $dir --time $time --maxq $qsize --cong cubic
 python plot_queue.py -f $dir/q.txt -o $dir/cubicq.png
 python plot_ping.py -f $dir/ping.txt -o $dir/cubicrtt.png
 
@@ -43,7 +43,7 @@ python tcp.py --bw-net $bwnet --delay $delay --dir $dir --time $time --maxq $qsi
 python plot_queue.py -f $dir/q.txt -o $dir/qbbr1.png
 python plot_ping.py -f $dir/ping.txt -o $dir/rttbbr1.png
 
-python tcp.py --bw-host 100--bw-net $bwnet --delay $delay --dir $dir --time $time --maxq $qsize --cong bbr --qman fq
+python tcp.py --bw-host 100 --bw-net $bwnet --delay $delay --dir $dir --time $time --maxq $qsize --cong bbr --qman fq
 python plot_queue.py -f $dir/q.txt -o $dir/bbrq.png
 python plot_ping.py -f $dir/ping.txt -o $dir/bbrrtt.png
 
